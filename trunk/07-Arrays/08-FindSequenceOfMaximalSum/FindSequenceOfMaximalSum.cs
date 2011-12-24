4d10afec7017b6fdf7ca3cad75233facc917786f
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-24 17:53:59 +0000

    Arrays: Exercise 8 - FindSequenceOfMaximalSum Added

---
+++ trunk/07-Arrays/08-FindSequenceOfMaximalSum/FindSequenceOfMaximalSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _08_FindSequenceOfMaximalSum
+{
+    class FindSequenceOfMaximalSum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write("array[{0}] = ", index);
+                array[index] = Int32.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            int bestSum = Int32.MinValue;
+            int counter = 0;
+            int firstIndex = 0;
+            for (int index = 0; index < array.Length; index++)
+            {
+                sum += array[index];
+                counter++;
+                if(sum < 0)
+                {
+                    sum = 0;
+                    counter = 0;
+                }
+                else if(sum > bestSum)
+                {
+                    bestSum = sum;
+                    firstIndex = index - counter;
+                }
+            }
+
+            for (int index = firstIndex + 1; index < counter + firstIndex; index++)
+            {
+                Console.Write("{0} ", array[index]);
+            }
+        }
+    }
+}
