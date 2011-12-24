4ee6332e3acbb63a3b15aaaba498b98936acafb1
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-24 19:49:19 +0000

    Arrays: Exercise 9 - TheMostFrequentNumber Added

---
+++ trunk/07-Arrays/09-TheMostFrequentNumber/TheMostFrequentNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09_TheMostFrequentNumber
+{
+    class TheMostFrequentNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write("array[{0}] = ", index);
+                array[index] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(array);
+
+            int counter = 1;
+            int mostFrequentNumber = 0;
+            int frequencyCount = Int32.MinValue;
+            for (int index = 0; index < array.Length - 1; index++)
+            {
+                if (array[index] == array[index + 1])
+                {
+                    counter++;
+                    if (frequencyCount < counter)
+                    {
+                        mostFrequentNumber = array[index];
+                        frequencyCount = counter;
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+
+            Console.WriteLine("The most frequent number is: {0} with {1} times containing.", 
+                mostFrequentNumber, frequencyCount);
+        }
+    }
+}
