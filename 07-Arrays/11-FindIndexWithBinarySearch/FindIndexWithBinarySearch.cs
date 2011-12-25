4e35fd5b427609a15e35f08957ff15ae99b878bd
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-25 16:21:45 +0000

    Arrays: Exercise 11 - FindIndexWithBinarySearch Added

---
+++ 07-Arrays/11-FindIndexWithBinarySearch/FindIndexWithBinarySearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _11_FindIndexWithBinarySearch
+{
+    class FindIndexWithBinarySearch
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Element = ");
+            int element = Int32.Parse(Console.ReadLine());
+
+            int[] sortedArray = new int[n];
+            for (int index = 0; index < sortedArray.Length; index++)
+            {
+                Console.Write("array[{0}] =", index);
+                sortedArray[index] = Int32.Parse(Console.ReadLine());
+            } 
+            Array.Sort(sortedArray);
+
+            Console.WriteLine("Sorted array: ");
+            for (int index = 0; index < sortedArray.Length; index++)
+            {
+                Console.WriteLine("sortedArray[{0}] = {1}", index, sortedArray[index]);
+            }
+
+            int elementIndex = BinarySearch(sortedArray, element, 0, sortedArray.Length);
+            Console.WriteLine("The index of {0} is: {1}",
+                element, elementIndex);
+        }
+
+        private static int BinarySearch(int[] array, int element, int min, int max)
+        {
+            if(max < min)
+            {
+                return -1;
+            }
+            int middle = (min + max) / 2;
+
+            if(array[middle] > element)
+            {
+                return BinarySearch(array, element, min, middle - 1);
+            }
+            else if(array[middle] < element)
+            {
+                return BinarySearch(array, element, middle + 1, max);
+            }
+            else
+            {
+                return middle;
+            }
+        }
+    }
+}
