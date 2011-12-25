f96b8a76a23445014ca569710cca757c1a8155a1
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-25 17:00:39 +0000

    Arrays: Exercise 12 - PrintIndexOfLetters Added

---
+++ 07-Arrays/12-PrintIndexOfLetters/PrintIndexOfLetters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12_PrintIndexOfLetters
+{
+    class PrintIndexOfLetters
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = new char[26];
+
+            for (int index = 0; index < letters.Length; index++)
+            {
+                letters[index] = (char)(index + 65);
+            }
+
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if(word.ToUpper()[i] == letters[j])
+                    {
+                        Console.WriteLine("The inddex of {0} in the array is: {1}",
+                            word.ToUpper()[i], j);
+                    }
+                }
+            }
+        }
+    }
+}
