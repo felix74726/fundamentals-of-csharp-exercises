fdf3e8bf95181ccc7f2fea35b275f564d4499eb0
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-28 13:50:58 +0000

    Arrays: Exercise 13 - MergeSort Added

---
+++ 07-Arrays/13-MergeSort/MergeSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _13_MergeSort
+{
+    class MergeSort
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = n - 1; j >= i; --j)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        array[j - 1] ^= array[j];
+                        array[j] ^= array[j - 1];
+                        array[j - 1] ^= array[j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+        }
+    }
+}
