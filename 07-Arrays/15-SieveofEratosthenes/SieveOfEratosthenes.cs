4762b064f2d8cace1ec5bad1183dbd8721872563
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-29 18:26:46 +0000

    Arrays: Exercise 15 - SieveOfEratosthenes Added

---
+++ 07-Arrays/15-SieveofEratosthenes/SieveOfEratosthenes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _15_SieveofEratosthenes
+{
+    class SieveOfEratosthenes
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000000;
+            double biggestSquareRoot = Math.Sqrt(n);
+            bool[] eliminated = new bool[n];
+
+            List<long> primeList = new List<long>();
+            primeList.Add(2);
+            for (int i = 3; i <= n; i += 2)
+            {
+                if (!eliminated[i])
+                {
+                    if (i < biggestSquareRoot)
+                    {
+                        for (int j = i * i; j <= n; j += 2 * i)
+                        {
+                            eliminated[j] = true;
+                        }
+                    }
+                    primeList.Add(i);  
+                }
+            }
+
+            foreach (var item in primeList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
