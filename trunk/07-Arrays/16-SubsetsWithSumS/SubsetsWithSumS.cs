9b321757fb8bd95cf5b1977f79deb471f1e9efee
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2011-12-30 13:31:26 +0000

    Arrays: Exercise 17 - 1SubsetOfKElemetdsWithSumS Added

---
+++ trunk/07-Arrays/16-SubsetsWithSumS/SubsetsWithSumS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16_SubsetsWithSumS
+{
+    class SubsetsWithSumS
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 1, 2, 4, 3, 5, 2, 6 };
+            int s = 14;
+        }
+    }
+}
