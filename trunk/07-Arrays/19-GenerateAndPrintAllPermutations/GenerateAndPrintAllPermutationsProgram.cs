d05f46bf0f7e738dde4c93571926542d1a827fb6
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-02 11:18:31 +0000

    Arrays: Exercise 19 - GenerateAndPrintAllPermutationsProgram Added

---
+++ trunk/07-Arrays/19-GenerateAndPrintAllPermutations/GenerateAndPrintAllPermutationsProgram.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _19_GenerateAndPrintAllPermutations
+{
+    class GenerateAndPrintAllPermutationsProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            GeneratePermutations(array, 0, array.Length - 1);
+        }
+
+        static void GeneratePermutations(int[] array, int k, int m)
+        {
+            if (k == m)
+            {
+                PrintAllPermutations(m, array);
+            }
+            else
+            {
+                for (int i = k; i <= m; i++)
+                {
+                    if (array[i] != array[k])
+                    {
+                        Swap(ref array[k], ref array[i]);
+                        GeneratePermutations(array, k + 1, m);
+                        Swap(ref array[k], ref array[i]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            if (a == b)
+            {
+                return;
+            }
+            else
+            {
+                a ^= b;
+                b ^= a;
+                a ^= b;
+            }
+        }
+
+        private static void PrintAllPermutations(int m, int[] array)
+        {
+            for (int i = 0; i <= m; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
