ab1f73e9d9aeec90a0b94af708f0a9f32059a37a
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-03 21:50:15 +0000

    MultidimensionalArrays: Exercise 4 - FindBiggerNumberEqualOrLessK Added

---
+++ trunk/08-Multidimensional Arrays/04-FindBiggerNumberEqualOrLessK/FindBiggerNumberEqualOrLessK.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04_FindBiggerNumberEqualOrLessK
+{
+    class FindBiggerNumberEqualOrLessK
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("K = ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[{0}] = ", i);
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            Array.Sort(array);
+
+            int index = 0;
+            while (true)
+            {
+                index = Array.BinarySearch(array, k);
+                if (index < 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("The largest number <= k is: {0}", array[index]);
+        }
+    }
+}
