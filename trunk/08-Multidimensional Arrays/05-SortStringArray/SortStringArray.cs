ea2a68f32730e00618a112bd7174bfa8074187ad
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-03 21:25:16 +0000

    MultidimensionalArrays: Exercise 5 - SortStringArray Added

---
+++ trunk/08-Multidimensional Arrays/05-SortStringArray/SortStringArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_SortStringArray
+{
+    class SortStringArray
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "hi", "hello", "some", "text", "a", "interesting", "b" };
+
+            SortStringArrayByLength(array);
+        }
+
+        static void SortStringArrayByLength(string[] array)
+        {
+            var arrayAnscending =
+                from arr in array
+                orderby arr.Length
+                select arr;
+
+            foreach (var arr in arrayAnscending)
+            {
+                Console.WriteLine(arr);
+            }
+        }
+    }
+}
