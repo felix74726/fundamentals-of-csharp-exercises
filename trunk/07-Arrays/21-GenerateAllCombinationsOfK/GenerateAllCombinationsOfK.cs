494c360f0e0e0ecb627e4151f6c576fdf393d380
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-04 15:42:42 +0000

    Arrays: Exercise 20 - GenerateAllVariationsOfK Modify

---
+++ trunk/07-Arrays/21-GenerateAllCombinationsOfK/GenerateAllCombinationsOfK.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _21_GenerateAllCombinationsOfK
+{
+    class GenerateAllCombinationsOfK
+    { 
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int k = 2;
+
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            GenerateAllCombinations(array, 0, array.Length - 1, k - 1);
+        }
+
+        static void GenerateAllCombinations(int[] array, int index, int length, int k)
+        {
+            if (index == length)
+            {
+                PrintAllPermutations(k, array);
+            }
+            else
+            {
+                for (int i = index; i <= length; i++)
+                {
+                    if (array[length] == k)
+                    {
+                        break;
+                    }
+
+                    Swap(ref array[index], ref array[i]);
+                    GenerateAllCombinations(array, index + 1, length, k);
+                    Swap(ref array[index], ref array[i]);
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            if (a == b)
+            {
+                return;
+            }
+            else
+            {
+                a ^= b;
+                b ^= a;
+                a ^= b;
+            }
+        }
+
+        private static void PrintAllPermutations(int length, int[] array)
+        {
+            for (int i = 0; i <= length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
