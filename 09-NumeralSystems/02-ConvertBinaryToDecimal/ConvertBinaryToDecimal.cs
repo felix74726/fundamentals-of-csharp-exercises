80a9fdd0ebb7e9b425401d9f9064355cea2695d7
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-05 20:57:47 +0000

    NumeralSystems: Exercise 2 - ConvertBinaryToDecimal Added

---
+++ 09-NumeralSystems/02-ConvertBinaryToDecimal/ConvertBinaryToDecimal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02_ConvertBinaryToDecimal
+{
+    class ConvertBinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a binary number: ");
+            string binaryNumber = Console.ReadLine();
+
+            char[] bits = binaryNumber.ToCharArray();
+            ConvertBinaryNumberToDecimal(bits);
+        }
+
+        private static void ConvertBinaryNumberToDecimal(char[] bits)
+        {
+            int decimalNumber = 0;
+            int multiplayer = 1;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (i != 0)
+                {
+                    multiplayer *= 2;
+                }
+                if (bits[bits.Length - 1 - i] == '1')
+                {
+                    if (i == 0)
+                    {
+                        decimalNumber += 1;
+                    }
+                    else
+                    {
+                        decimalNumber += multiplayer;
+                    }
+                }
+            }
+            Console.WriteLine(decimalNumber);
+        }
+    }
+}
