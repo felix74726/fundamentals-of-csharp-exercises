6849276f3a3bb959017658e3143357ef4003ddc5
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-05 17:58:04 +0000

    NumeralSystems: Exercise 1 - ConvertDecimalToBinary Added

---
+++ 09-NumeralSystems/01-ConvertDecimalToBinary/ConvertDecimalToBinary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01_ConvertDecimalToBinary
+{
+    class ConvertDecimalToBinary
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a decimal number: ");
+            int decimalNumber = Int32.Parse(Console.ReadLine());
+
+            ConvertDecimalNumberToBinary(decimalNumber);
+        }
+
+        private static void ConvertDecimalNumberToBinary(int decimalNumber)
+        {
+            StringBuilder reversedBinaryNumber = new StringBuilder();
+            int number = decimalNumber;
+            while (decimalNumber != 0)
+            {
+                if (decimalNumber % 2 == 0)
+                {
+                    reversedBinaryNumber.Append("0");
+                }
+                else
+                {
+                    reversedBinaryNumber.Append("1");
+                }
+
+                decimalNumber /= 2;
+            }
+
+            StringBuilder binaryNumber = new StringBuilder();
+            for (int i = reversedBinaryNumber.Length - 1; i >= 0; i--)
+            {
+                binaryNumber.Append(reversedBinaryNumber[i]);
+            }
+
+            Console.WriteLine("Binary representation of {0} is {1}",
+                number, binaryNumber);
+        }
+    }
+}
