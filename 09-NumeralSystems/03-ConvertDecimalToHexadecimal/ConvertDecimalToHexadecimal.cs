8ace124434396a01288faf6b3347550f2caa0e47
tsvetomir.kirchev@gmail.com <tsvetomir.kirchev@gmail.com@4c0dfb5b-b68b-813e-4162-9779c865afc2>
2012-01-10 09:51:45 +0000

    NumeralSystems: Exercise 3 - ConvertDecimalToHexadecimal Added

---
+++ 09-NumeralSystems/03-ConvertDecimalToHexadecimal/ConvertDecimalToHexadecimal.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _03_ConvertDecimalToHexadecimal
+{
+    class ConvertDecimalToHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a decimal number: ");
+            int decimalNumber = Int32.Parse(Console.ReadLine());
+
+            DecimalToHexadecimal(decimalNumber);
+        }
+
+        static void DecimalToHexadecimal(int decimalNumber)
+        {
+            StringBuilder reversedHexaDecimalNumber = new StringBuilder();
+            int number;
+            while(decimalNumber != 0)
+            {
+                number = decimalNumber % 16;
+                reversedHexaDecimalNumber.Append(HexadecimalCharacter(number));
+                decimalNumber /= 16;
+            }
+
+            StringBuilder hexaDecimalNumber = new StringBuilder();
+            for (int i = reversedHexaDecimalNumber.Length - 1; i >= 0; i--)
+            {
+                hexaDecimalNumber.Append(reversedHexaDecimalNumber[i]);
+            }
+            Console.WriteLine(hexaDecimalNumber);
+        }
+
+        private static char HexadecimalCharacter(int num)
+        {
+            char character = ' ';
+            switch(num)
+            {
+                case 0:
+                    character = '0';
+                    break;
+                case 1:
+                    character = '1';
+                    break;
+                case 2:
+                    character = '2';
+                    break;
+                case 3:
+                    character = '3';
+                    break;
+                case 4:
+                    character = '4';
+                    break;
+                case 5:
+                    character = '5';
+                    break;
+                case 6:
+                    character = '6';
+                    break;
+                case 7:
+                    character = '7';
+                    break;
+                case 8:
+                    character = '8';
+                    break;
+                case 9:
+                    character = '9';
+                    break;
+                case 10:
+                    character = 'A';
+                    break;
+                case 11:
+                    character = 'B';
+                    break;
+                case 12:
+                    character = 'C';
+                    break;
+                case 13:
+                    character = 'D';
+                    break;
+                case 14:
+                    character = 'E';
+                    break;
+                case 15:
+                    character = 'F';
+                    break;
+                default:
+                    Console.WriteLine("Error. The input chars in HexadecimalCharacter are not correct!");
+                    break;
+            }
+            return character;
+        }
+    }
+}
